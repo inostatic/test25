a5384904db4a9efc50dd1574f714eaa035eba0b2
bogdanovso <inostc@bk.ru>
2019-09-25 23:51:17 +0300

    фиксирую потом выталкиваю

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
-
-echo "My first experience";
-echo "My first experience";
-echo "My first experience";
-echo "My first experience";
+$text = "My first expirience";
+?>
+<html>
+    <head>
+        <title>NewHope</title>
+        <meta charset="utf8">
+    </head>
+    <body>
+    <center><h1><b><?php echo $text; ?></b></h1></center>
+    </body>
+</html>
