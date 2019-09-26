e814d33bce958f6538524da785f6da1a98c60ae2
bogdanovso <inostc@bk.ru>
2019-09-26 23:12:38 +0300

    фиксирую потом выталкиваю

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 //Подключаем роутер
 define('ROOT', dirname(__FILE__));
 require_once(ROOT.'/components/Router.php');
-require_once(ROOT.'/config/db.php');
+require_once(ROOT.'/components/db.php');
 
 //Вызываем нужный метод 
 $router = new Router();
