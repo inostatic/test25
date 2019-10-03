42a3747d9594da0285427d5d03e73f1d229e2b35
bogdanovso <inostc@bk.ru>
2019-10-03 23:26:34 +0300

    Регистрация работает.Надо сделать админку

--- views/form/formEntered.php
+++ views/form/formEntered.php
@@ -1,3 +1,3 @@
 
-<p><?php echo "Здравтсвуйте !".$_SESSION['session_username']['full_name']; ?></p>
+<p><?php echo "Здравствуйте !".$_SESSION['session_username']['full_name']; ?></p>
 <a href="../components/logout.php">Выйти</a>
